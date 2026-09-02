8e9b18885a200c8891a5a3c751b0181c5c465871
keontolentino <100613449+keontolentino@users.noreply.github.com>
2026-09-02 08:14:30 -0400

    Bulk sync: 20 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        arr = []
+        for i in range(len(nums)):
+            for j in range(1, len(nums)):
+                if (nums[i] + nums[j]) == target and i != j:
+                    arr.append(i)
+                    arr.append(j)
+                    return arr
+
+            
+                    
---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapper = {")":"(", "]":"[", "}":"{"}
+
+        for c in s:
+            if c in mapper:
+                if stack and stack[-1] == mapper[c]:
+                    stack.pop()
+                else: 
+                    return False
+            else:
+                stack.append(c)
+        
+        return True if not stack else False
+
+            
